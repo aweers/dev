76e67af7d9f637a9932397066203a12593b5b717
Alexander Weers <mail@aweers.de>
2025-05-14 22:39:03 +0200

    add git aliases

---
+++ runs/git.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# set aliases
+git config --global alias.st "status -s"
+git config --global alias.sta "status"
+git config --global alias.co "commit -m"
+git config --global alias.a "add"
+git config --global alias.aa "add -A"
+git config --global alias.res "restore --staged"
+git config --global alias.di "diff"
+git config --global alias.dis "diff --staged"
